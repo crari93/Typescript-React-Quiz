abbfd68eaa14e2396f3f12a3537b59e4d883f8d5
Ariel Rodríguez Boza <33108087+crari93@users.noreply.github.com>
2021-01-25 12:51:44 -0600

    Updated some code and added some new files

--- src/components/QuestionCard.tsx
+++ src/components/QuestionCard.tsx
@@ -1,5 +1,35 @@
-import React from 'react'
+import React from "react";
 
-const QuestionCard = () => <div>Question Card</div>
+type Props = {
+  question: string;
+  answers: string[];
+  callback: any;
+  userAnswer: boolean;
+  questionNr: number;
+  totalQuestions: number;
+};
 
-export default QuestionCard
+const QuestionCard: React.FC<Props> = ({
+  question,
+  answers,
+  callback,
+  userAnswer,
+  questionNr,
+  totalQuestions,
+}) => (
+  <div>
+    <p className="number">
+      Questioon: {questionNr} / {totalQuestions}
+    </p>
+    <p dangerouslySetInnerHTML={{ __html: question }}></p>
+    {answers.map((answer) => (
+      <div>
+        <button disabled={userAnswer} onClick={callback}>
+          <span dangerouslySetInnerHTML={{ __html: answer }}></span>
+        </button>
+      </div>
+    ))}
+  </div>
+);
+
+export default QuestionCard;
